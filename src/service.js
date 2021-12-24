e6aa36720bc06448d93563790ab212d45d18b3ad
Max <maximlev01234@gmail.com>
2021-12-24 05:29:32 +0200

    changed url for backend

--- src/service.js
+++ src/service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://52.188.42.170/api/getalldata";
+const url = "https://lab3-func.azurewebsites.net/api/getalldata";
 
 const getAllData = async (setData) => {
     axios.get(url).then(
